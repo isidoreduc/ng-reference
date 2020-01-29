34a49978fd0d16584c28246aabc1538de5a30217
isidoreduc <isidoreducasse79@gmail.com>
2020-01-29 15:38:41 +0100

    model interface

--- src/app/src/app/products/products.component.ts
+++ src/app/src/app/products/products.component.ts
@@ -1,40 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { IProduct } from "src/app/model/product";
 
 @Component({
-  selector: 'app-products',
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  selector: "app-products",
+  templateUrl: "./products.component.html",
+  styleUrls: ["./products.component.css"]
 })
 export class ProductsComponent implements OnInit {
-  pageTitle = 'Product List';
+  pageTitle = "Product List";
   imageHeight = 30;
   showImage = false;
-  listFilter = 'cart';
-  products: any[] = [
+  listFilter = "cart";
+  products: IProduct[] = [
     {
-      prodId: 2,
-      prodName: 'Garden Cart',
-      prodCode: 'GDN-003',
-      releaseDate: '13 March 2019',
-      description: '15 gallon capacity rolling garden cart',
+      productId: 2,
+      productName: "Garden Cart",
+      productCode: "GDN-003",
+      releaseDate: "March 18, 2019",
+      description: "15 gallon capacity rolling garden cart",
       price: 32.99,
-      rating: 4.2,
-      imageUrl: 'assets/images/garden_cart.png'
+      starRating: 4.2,
+      imageUrl: "assets/images/garden_cart.png"
     },
     {
-      prodId: 5,
-      prodName: 'Hammer',
-      prodCode: 'GDN-004',
-      releaseDate: '18 March 2019',
-      description: '15 pounds heavy hammer',
+      productId: 5,
+      productName: "Hammer",
+      productCode: "GDN-004",
+      releaseDate: "18 March 2019",
+      description: "15 pounds heavy hammer",
       price: 12.99,
-      rating: 3.2,
-      imageUrl: 'assets/images/hammer.png'
+      starRating: 3.2,
+      imageUrl: "assets/images/hammer.png"
     }
   ];
   constructor() {}
 
   ngOnInit() {}
 
-  toggleImage = () => this.showImage = !this.showImage;
+  toggleImage = () => (this.showImage = !this.showImage);
 }
