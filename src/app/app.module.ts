34af8783a3ddb530f09eba9e4dab7518dab0ae50
isidoreduc <isidoreducasse79@gmail.com>
2020-02-04 19:22:22 +0100

    implementing route guards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
 
-import { AppComponent } from './app.component';
-import { ProductsComponent } from './products/products.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces';
-import { StarComponent } from './shared/star/star.component';
-import { HttpClientModule } from '@angular/common/http';
-import { ProductDetailComponent } from './products/product-detail.component';
-import { WelcomeComponent } from './welcome/welcome.component';
+import { AppComponent } from "./app.component";
+import { ProductsComponent } from "./products/products.component";
+import { ConvertToSpacesPipe } from "./shared/convert-to-spaces";
+import { StarComponent } from "./shared/star/star.component";
+import { HttpClientModule } from "@angular/common/http";
+import { ProductDetailComponent } from "./products/product-detail.component";
+import { WelcomeComponent } from "./welcome/welcome.component";
+import { ProductDetailGuard } from "./products/product-detail.guard";
 
 @NgModule({
   declarations: [
@@ -26,11 +27,15 @@ import { WelcomeComponent } from './welcome/welcome.component';
     HttpClientModule,
     RouterModule.forRoot(
       [
-        { path: 'products', component: ProductsComponent },
-        { path: 'products/:id', component: ProductDetailComponent },
-        { path: 'welcome', component: WelcomeComponent },
-        { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-        { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+        { path: "products", component: ProductsComponent },
+        {
+          path: "products/:id",
+          canActivate: [ProductDetailGuard],
+          component: ProductDetailComponent
+        },
+        { path: "welcome", component: WelcomeComponent },
+        { path: "", redirectTo: "welcome", pathMatch: "full" },
+        { path: "**", redirectTo: "welcome", pathMatch: "full" }
       ],
       { useHash: true }
     )
