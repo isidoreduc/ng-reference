e7bfec0ff5c4c77b7c66a9dfff40f5414aaf5044
isidoreduc <isidoreducasse79@gmail.com>
2020-02-04 15:40:02 +0100

    product detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { ProductsComponent } from './products/products.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces';
 import { StarComponent } from './shared/star/star.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ProductDetailComponent } from './products/product-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductsComponent, ConvertToSpacesPipe, StarComponent],
+  declarations: [AppComponent, ProductsComponent, ConvertToSpacesPipe, StarComponent, ProductDetailComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
